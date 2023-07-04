27ce69f8592c497c0d45303bdc972baf0b4e3939
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-04 15:35:59 +0100

    Update gestionusers.js

--- src/views/super_admin/utilisateurs/gestionusers.js
+++ src/views/super_admin/utilisateurs/gestionusers.js
@@ -125,7 +125,7 @@ const UtilisateursListe = () => {
     }, []);
 
     const fetchAllUser = () => {
-        http.get('/user').then(res => {
+        http.get('/user_for_super_admin').then(res => {
             setUser(res.data);
         })
     }
