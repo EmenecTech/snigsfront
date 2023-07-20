91197ef1cd07826dcf5710c68530bb41b314a4ce
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-20 13:37:38 +0100

    Update notes.js

--- src/views/fondateur/notes/notes.js
+++ src/views/fondateur/notes/notes.js
@@ -37,6 +37,12 @@ const AdminEditNotes = () => {
         fetchAllmatiere_classe_info();
     }, []);
 
+    const fetchAllmatiere_classe_info = () => {
+        http.get('/get_info_matiere_classe/' + etab + '/' + classe + '/' + matiere).then(res => {
+            setmatiere_classe_info(res.data);
+        })
+    }
+
     const coefficient = matiere_classe_info.coefficient_cm;
 
     // useEffect(()=>{
