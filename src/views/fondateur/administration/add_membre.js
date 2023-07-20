513f6b3376009fb0175975ac138bba2af34f2ca9
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-20 14:49:27 +0100

    Update add_membre.js

--- src/views/fondateur/administration/add_membre.js
+++ src/views/fondateur/administration/add_membre.js
@@ -52,7 +52,7 @@ const AddMembreAdministration = () => {
                         <Card>
                             <Card.Header className="d-flex justify-content-between">
                                 <div className="header-title">
-                                    <h4 className="card-title">Ajouter un membre de l'administration</h4>
+                                    <h4 className="card-title">{user.langue === "en" ? (<div>Add a member of the administration</div>):(<div> Ajouter un membre de l'administration </div>)}</h4>
                                 </div>
                             </Card.Header>
                             <Card.Body>
@@ -62,7 +62,7 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Nom </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1">{user.langue === "en" ? (<div>Name<div>):(<div> Nom  </div>)}</Form.Label>
                                                     <Form.Control type="text" defaultValue="" name="nom" onChange={handleChange} required />
                                                 </Form.Group>
                                             </Form.Group>
@@ -70,8 +70,8 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Prénom </Form.Label>
-                                                    <Form.Control type="text" defaultValue="" name="prenom" onChange={handleChange} required />
+                                                    <Form.Label htmlFor="exampleInputText1"> {user.langue === "en" ? (<div>Surname<div>):(<div> Prénom </div>)} </Form.Label>
+                                                    <Form.Control type="text" defaultValue="" name="" onChange={handleChange} required />
                                                 </Form.Group>
                                             </Form.Group>
                                         </Col>
@@ -79,11 +79,11 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Genre </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1">{user.langue === "en" ? (<div>Gender<div>):(<div> Genre  </div>)} </Form.Label>
                                                     <select className="form-select mb-3 shadow-none" name="genre" onChange={handleChange}>
                                                         <option></option>
-                                                        <option value="Masculin">Masculin</option>
-                                                        <option value="Feminin">Feminin</option>
+                                                        <option value="M">{user.langue === "en" ? (<div>Male<div>):(<div> Masculin  </div>)} </option>
+                                                        <option value="F">{user.langue === "en" ? (<div>Female<div>):(<div> Féminin </div>)}</option>
                                                     </select>
                                                 </Form.Group>
                                             </Form.Group>
@@ -94,7 +94,7 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Date de naissance </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1"> {user.langue === "en" ? (<div>Date of birth<div>):(<div> Date de naissance </div>)}</Form.Label>
                                                     <Form.Control type="date" defaultValue="" name="date_n" onChange={handleChange} />
                                                 </Form.Group>
                                             </Form.Group>
@@ -102,7 +102,7 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Lieu de naissance </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1"> {user.langue === "en" ? (<div>Place of birth<div>):(<div> Lieu de naissance</div>)}</Form.Label>
                                                     <Form.Control type="text" defaultValue="" name="lieu_n" onChange={handleChange} required />
                                                 </Form.Group>
                                             </Form.Group>
@@ -111,7 +111,7 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Nationalité </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1">{user.langue === "en" ? (<div>Nationality<div>):(<div> Nationalité </div>)}</Form.Label>
                                                     <Form.Control type="text" defaultValue="" name="nation" onChange={handleChange} required />
                                                 </Form.Group>
                                             </Form.Group>
@@ -132,7 +132,7 @@ const AddMembreAdministration = () => {
                                         <Col>
                                             <Form.Group as={Row} className="form-group">
                                                 <Form.Group className="form-group">
-                                                    <Form.Label htmlFor="exampleInputText1">Téléphone </Form.Label>
+                                                    <Form.Label htmlFor="exampleInputText1"> {user.langue === "en" ? (<div>Phone<div>):(<div> Téléphone</div>)} </Form.Label>
                                                     <Form.Control type="text" defaultValue="" name="tel" onChange={handleChange} required />
                                                 </Form.Group>
                                             </Form.Group>
