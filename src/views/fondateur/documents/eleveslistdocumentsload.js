0c6862aceda08f7e0d9e4f32eed2e7b79a3c496b
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-20 18:30:46 +0100

    Update eleveslistdocumentsload.js

--- src/views/fondateur/documents/eleveslistdocumentsload.js
+++ src/views/fondateur/documents/eleveslistdocumentsload.js
@@ -427,7 +427,7 @@ const AdminlistdocumentsElevesLoad = memo((props) => {
 
 
                                         <div className="text-center">
-                                            <Button type="button" variant="primary" onClick={submitForm} >Confirmer</Button>
+                                            <Button type="button" variant="primary" onClick={submitForm} >{user.langue === "en" ? (<div>Confirm</div>):(<div>Confirmer</div>)}</Button>
                                         </div>
                                     </Form>
                                 </Modal.Body>
