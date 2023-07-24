0eef4a4c4e114bd9f8dc8dafc03c79fc171c2006
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-24 12:51:51 +0100

    Update edit_etablissement.js

--- src/views/fondateur/etablissement/edit_etablissement.js
+++ src/views/fondateur/etablissement/edit_etablissement.js
@@ -50,6 +50,9 @@ const EditEtablissement = () => {
 
     };
 
+
+       
+
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
