3708240a615c5ca86905028110c3c20a5f6adb21
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-25 09:41:30 +0100

    Update list_membres.js

--- src/views/fondateur/administration/list_membres.js
+++ src/views/fondateur/administration/list_membres.js
@@ -157,7 +157,7 @@ const MembresAdministration = () => {
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                             </svg>
                                         </span>
-                                        (<div>Add</div>):(<div> Ajouter </div>)}
+                                        {user.langue === "en" ? (<div>Add</div>):(<div> Ajouter </div>)}
                                     </Button>
                                 </Link>
                             </Card.Header>
