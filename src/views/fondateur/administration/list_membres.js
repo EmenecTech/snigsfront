b9631d5b639acdabfbbf3651f293378611a16c6f
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-25 09:37:56 +0100

    Update list_membres.js

--- src/views/fondateur/administration/list_membres.js
+++ src/views/fondateur/administration/list_membres.js
@@ -157,7 +157,7 @@ const MembresAdministration = () => {
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                             </svg>
                                         </span>
-                                        Ajouter
+                                        (<div>Add</div>):(<div> Ajouter </div>)}
                                     </Button>
                                 </Link>
                             </Card.Header>
