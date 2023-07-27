01787f124a05bd4c2bfa6a944c871b7bc0c58bc2
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-07-27 12:24:34 +0100

    Update editpayements.js

--- src/views/fondateur/comptabilite/editpayements.js
+++ src/views/fondateur/comptabilite/editpayements.js
@@ -514,8 +514,8 @@ const EditPensions = memo((props) => {
                             </Modal>
                         </Card.Header>
                         <Card.Body>
-                                //27/07/2023
-                            <Button variant="Warning mt-2" onClick={handleShow2}>
+                                
+                            <Button variant="warning mt-2" onClick={handleShow2}>
                                 <span className="btn-inner">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
