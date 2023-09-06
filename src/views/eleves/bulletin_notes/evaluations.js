e96b18ace1047001bdb0aa20611e63228fb3c684
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-09-06 16:46:21 +0200

    Update evaluations.js

--- src/views/eleves/bulletin_notes/evaluations.js
+++ src/views/eleves/bulletin_notes/evaluations.js
@@ -338,8 +338,7 @@ const EleveListEvaluations = memo((props) => {
                                             <th>Intitulé</th>
                                             <th>Type d'évaluation</th>
                                             <th>Type de période</th>
-                                            <th>Date de début</th>
-                                            <th>Date de fin</th>
+                                           
                                             <th>Action</th>
                                         </tr>
                                     </thead>
@@ -350,8 +349,7 @@ const EleveListEvaluations = memo((props) => {
 
                                                 <td>{item.type_evaluation_evaluation}</td>
                                                 <td>{item.reference_evaluation}</td>
-                                                <td>/</td>
-                                                <td>/</td>
+                                                
                                                 <td>
                                                     <Link className="btn btn-sm btn-icon btn-info" data-toggle="tooltip" data-placement="top" title="Delete" data-original-title="Delete" to={"/Eleve/Bulletin/Notes/" + item.intitule_evaluation}>
 
