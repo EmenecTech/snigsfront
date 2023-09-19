f2f417fd0b0b74f73023eefe600c5b03c708f120
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-09-19 12:08:14 +0100

    Update editenseignants.js

--- src/views/fondateur/enseignants/editenseignants.js
+++ src/views/fondateur/enseignants/editenseignants.js
@@ -31,7 +31,7 @@ const EditEnseignants = () => {
     }, []);
 
     const fetchenseignants = () => {
-        http.get("/editens/" + id ).then((res) => {
+        http.put("/edituser/" + id ).then((res) => {
             setInputs({
                 tel: res.data.telephone,
                 nom: res.data.nom,
