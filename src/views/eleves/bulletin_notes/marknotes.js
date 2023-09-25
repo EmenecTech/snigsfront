7e44288e49fa4faf4f16d2ac89f910f636bf9858
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-09-25 19:12:57 +0200

    Update marknotes.js

--- src/views/eleves/bulletin_notes/marknotes.js
+++ src/views/eleves/bulletin_notes/marknotes.js
@@ -1115,7 +1115,7 @@ const BulletinByEleve = memo((props) => {
 
                                                         <Col sm="12" lg="12">
                                                              <div className="mt-2">
-                                                                <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant: {enseign.users.nom} </p>
+                                                                <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant: </p>
                                                                
                                                             </div>
                                                             <div className="mt-2">
@@ -1470,7 +1470,7 @@ const BulletinByEleve = memo((props) => {
 
                                                         <Col sm="12" lg="12">
                                                            <div className="mt-2">
-                                                                <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant: {enseign.nom} </p>
+                                                                <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant:  </p>
                                                                 </div>
                                                             <div className="mt-2">
                                                                 <p tyle={{ fontSize: "10px" }} className="mb-0">Redoublant:</p>
