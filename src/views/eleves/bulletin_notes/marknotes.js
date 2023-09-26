5a7ec3f257ed49665fca0a0f110fb98ce9fc94ab
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-09-26 10:27:16 +0200

    Update marknotes.js

--- src/views/eleves/bulletin_notes/marknotes.js
+++ src/views/eleves/bulletin_notes/marknotes.js
@@ -204,7 +204,7 @@ const BulletinByEleve = memo((props) => {
              setenseign(res.data);
         })
     }
-    console.log(enseign.users.nom);
+    console.log(enseign);
 
    const [allnotes, setAllNotes] = useState([]);
     useEffect(() => {
