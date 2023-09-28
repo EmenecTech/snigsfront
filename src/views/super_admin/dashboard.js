df7b40816bcd9016f1b3b88e40617f890ad849c2
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-09-28 15:27:03 +0100

    Update dashboard.js

--- src/views/super_admin/dashboard.js
+++ src/views/super_admin/dashboard.js
@@ -10,7 +10,7 @@ import Circularprogressbar from "../../components/circularprogressbar.js";
 // AOS 
 import AOS from "aos";
 import "../../../node_modules/aos/dist/aos";
-import "../../../node_modules/aos/dist/aos.css";
+import "../../../node_modules/aos/dist/aos.css"; 
 //apexcharts
 import Chart from "react-apexcharts";
 
