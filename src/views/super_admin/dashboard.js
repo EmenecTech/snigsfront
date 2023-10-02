cf939b8a3b063a1474a359bdd70eae1c1017fb03
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-02 17:03:54 +0100

    Update dashboard.js

--- src/views/super_admin/dashboard.js
+++ src/views/super_admin/dashboard.js
@@ -342,6 +342,19 @@ const [nbretab_sup, setnbretab_sup] = useState([]);
             setnbrparsup(res.data);
         })
     }
+
+      
+const [sumprim, setsumprim] = useState([]);
+  useEffect(() => {
+    fetchAllsumprim();
+  }, []);
+
+  const fetchAllsumprim = () => {
+    http.get('/sum/prim/' + etab).then(res => {
+      setsumprim(res.data);
+    })
+  }
+
   const nbreleveins_sup = nbreleve_sup - nbrpreins_sup;
 
 
@@ -2182,7 +2195,7 @@ const [nbretab_sup, setnbretab_sup] = useState([]);
                           <div className="progress-detail">
                             <p className="mb-2">Etablissements Primaires</p>
                             <h4 className="counter">
-                              <CountUp start={0} end={0} duration={3} />
+                              <CountUp start={0} end={sumprim} duration={3} />
                             </h4>
                           </div>
                         </div>
