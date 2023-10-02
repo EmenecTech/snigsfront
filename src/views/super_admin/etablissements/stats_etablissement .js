6bc7773f166c53b77eba9de0d578b74ceaeb10de
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-02 16:41:41 +0100

    Update stats_etablissement .js

--- src/views/super_admin/etablissements/stats_etablissement .js
+++ src/views/super_admin/etablissements/stats_etablissement .js
@@ -391,16 +391,6 @@ const [sumpay, setsumpay] = useState([]);
                                 <h4 className="card-title">{nometab}</h4>
                             </div>
 
-                            <Link to="/etablissement/add/super/admin">
-                                <Button type="button" variant="primary">
-                                    <span className="btn-inner">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                        </svg>
-                                    </span>
-                                    Ajouter
-                                </Button>
-                            </Link>
                         </Card.Header>
                         <Card.Body>
 
