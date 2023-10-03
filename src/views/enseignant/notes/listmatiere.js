c8a9c0cab63c194bee10c1ed00fbce210841f7e8
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-03 16:12:21 +0200

    Update listmatiere.js

--- src/views/enseignant/notes/listmatiere.js
+++ src/views/enseignant/notes/listmatiere.js
@@ -77,8 +77,16 @@ const Enseignant_matieres_notes = memo((props) => {
         })
     }
 
+     const [matieresprim, setmatieresprim] = useState([]);
+    useEffect(() => {
+        fetchAllmatieresprim();
+    }, []);
 
-
+    const fetchAllmatieresprim = () => {
+        http.get('/get_matieres_enseignant_prim/' + etab + '/' + userid).then(res => {
+            setmatieresprim(res.data);
+        })
+    }
 
     const submitForm = () => {
         http.post('/classes', inputs).then((res) => {
@@ -93,7 +101,16 @@ const Enseignant_matieres_notes = memo((props) => {
         console.log(inputs);
 
     }
+   const [eleves_classe, seteleves_classe] = useState([]);
+    useEffect(() => {
+        fetchAlleleves_classe();
+    }, []);
 
+    const fetchAlleleves_classe = () => {
+        http.get('/fetch_eleves_in_classe/' + etab + '/' + classe).then(res => {
+            seteleves_classe(res.data);
+        })
+    }
 
 
 
@@ -323,14 +340,6 @@ const Enseignant_matieres_notes = memo((props) => {
     };
 
 
-
-
-
-
-
-
-
-
     return (
         <Fragment>
             <Row>
@@ -344,8 +353,10 @@ const Enseignant_matieres_notes = memo((props) => {
 
 
                         </Card.Header>
-                        <Card.Body>
-
+                      
+        
+        {eleves_classe.cycle_niveau === 'Secondaire' || eleves_classe.cycle_niveau === 'Secondary' ? <div>
+                <Card.Body>
                             <div className="table-responsive border-bottom my-3">
                                 <Table
                                     responsive
@@ -393,6 +404,61 @@ const Enseignant_matieres_notes = memo((props) => {
 
                             </div>
                         </Card.Body>
+                                       
+                       </di> : <div>
+                                       
+                        <Card.Body>
+                            <div className="table-responsive border-bottom my-3">
+                                <Table
+                                    responsive
+                                    striped
+                                    id="datatable"
+                                    className=""
+                                    data-toggle="data-table"
+                                >
+                                    <thead>
+                                        <tr>
+
+                                            <th>Classe </th>
+                                          
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {matieres.map((item) => (
+                                            <tr key={item.id}>
+                                                <td>{item.classe_cp}</td>
+                                               
+                                                <td>
+                                                    <div className="flex align-items-center list-user-action">
+
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Enseignant/List/Groupes/" + item.classe_cp }>
+                                                            <span className="btn-inner">
+                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                </svg>
+                                                            </span>
+                                                        </Link>
+
+                                                    </div>
+                                                </td>
+
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                    <tfoot>
+
+                                    </tfoot>
+                                </Table>
+
+                            </div>
+                        </Card.Body>
+                                        
+                      </di>}
+        
+                    
                     </Card>
                 </Col>
             </Row>
