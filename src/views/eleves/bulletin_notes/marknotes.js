90302e4d1df261daa49d38f9f01a5226f2169434
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-06 16:48:50 +0200

    Update marknotes.js

--- src/views/eleves/bulletin_notes/marknotes.js
+++ src/views/eleves/bulletin_notes/marknotes.js
@@ -962,7 +962,7 @@ const BulletinByEleve = memo((props) => {
                                                     </Row>
     
                                                    <Row>
-                                                  
+                                                  <col sm="12" lg="12">
                                                     <Row style={{ fontSize: "10px" }}>
                                                       <Col sm="4" lg="4">
                                                         <div className="mt-2">
@@ -993,7 +993,7 @@ const BulletinByEleve = memo((props) => {
 
                                                         </Col>
                                                     </Row>
-                                                
+                                                 </col>
                                                 </Row>
                                                     <Row>
                                                         <div className="table-responsive border-bottom my-3">
