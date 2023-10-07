520bba1972c99809cd277253631429017aa3670b
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-07 11:41:40 +0100

    Update listelevesforclasse.js

--- src/views/fondateur/documents/listelevesforclasse.js
+++ src/views/fondateur/documents/listelevesforclasse.js
@@ -283,7 +283,7 @@ const ListelevesforClasses = memo((props) => {
           <Card>
             <Card.Header className="d-flex justify-content-between">
               <div className="header-title">
-                <h4 className="card-title">Classes</h4>
+                <h4 className="card-title">Elèves</h4>
               </div>
             </Card.Header>
             <Card.Body>
