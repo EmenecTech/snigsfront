06ddfc23f74f1b4ee0ee16565032300171ead679
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-09 10:47:16 +0100

    Update listelevesforclasse.js

--- src/views/fondateur/documents/listelevesforclasse.js
+++ src/views/fondateur/documents/listelevesforclasse.js
@@ -1,6 +1,6 @@
 import { Fragment, memo, useEffect, useState } from "react";
 import { Button, Col, Form, Modal, Row, Table } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import AuthUser from "../../../components/AuthUser.js";
 
 //circular
@@ -42,62 +42,19 @@ const ListClassesFiche = memo((props) => {
   const navigate = useNavigate();
   const [inputs, setInputs] = useState({});
   const etab = user.etablissement;
-  const id_classe = user.classes;
+  const classe = useParams();
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
+  const [eleves_classe, seteleves_classe] = useState([]);
+    useEffect(() => {
+        fetchAlleleves_classe();
+    }, []);
 
-    setInputs((values) => ({ ...values, [name]: value, etab }));
-  };
-
-  const submitForm = () => {
-    http.post("/classes", inputs).then((res) => {
-      alert("Classe ajoutée avec succès !");
-      navigate("/List/classes/");
-      window.location.reload(false);
-    });
-
-    console.log(inputs);
-  };
-  const [niveaux, setniveaux] = useState([]);
-  useEffect(() => {
-    fetchAllniveaux();
-  }, []);
-
-  const fetchAllniveaux = () => {
-    http.get("/niveaux").then((res) => {
-      setniveaux(res.data);
-    });
-  };
-
-  const [filieres, setfilieres] = useState([]);
-  useEffect(() => {
-    fetchAllfilieres();
-  }, []);
-
-  const fetchAllfilieres = () => {
-    http.get("/get_filieres/" + etab).then((res) => {
-      setfilieres(res.data);
-    });
-  };
-
-  const [classes, setclasses] = useState([]);
-  useEffect(() => {
-    fetchAllclasses();
-  }, []);
-
-  const fetchAllclasses = () => {
-    http.get("/get_classes/" + etab).then((res) => {
-      setclasses(res.data);
-    });
-  };
-
-  const deleteClasses = (id) => {
-    http.delete("/delete_classes/" + etab + "/" + id).then((res) => {
-      fetchAllclasses();
-    });
-  };
+    const fetchAlleleves_classe = () => {
+        http.get('/fetch_eleves_in_classe/' + etab + '/' + classe).then(res => {
+            seteleves_classe(res.data);
+        })
+    };
+  
   useSelector(SettingSelector.theme_color);
 
   const getVariableColor = () => {
@@ -454,75 +411,60 @@ const ListClassesFiche = memo((props) => {
                   data-toggle="data-table"
                 >
                   <thead>
-                    <tr>
-                      <th>Intitulé</th>
-                      <th>Niveau</th>
-                      <th>Filière </th>
-                      <th>Cycle </th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
+                                                    <tr>
+                                                        <th>{user.langue === "en" ? (<div>Registration number</div>):(<div> Matricule </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Name</div>):(<div> Nom  </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Prename</div>):(<div> Prénom  </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Gender </div>):(<div> Genre  </div>)}</th>
+                                                        <th> {user.langue === "en" ? (<div>Date of birth </div>):(<div> Date de naissance  </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Place of birth </div>):(<div> Lieu de naissance   </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Parent name</div>):(<div> Nom du parent  </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Parent contact</div>):(<div> Contact du parent  </div>)}</th>
+                                                        <th>Email</th>
+                                                        <th> {user.langue === "en" ? (<div>Phone </div>):(<div> Téléphone   </div>)}</th>
+                                                        <th>Action</th>
+
+                                                    </tr>
+                                                </thead>
                   <tbody>
-                    {classes.map((item) => (
-                      <tr key={item.id}>
-                        <td>{item.intitule_classe}</td>
-                        <td>{item.niveau_classe}</td>
-                        <td>{item.filiere}</td>
-                        <td>{item.cycle_niveau}</td>
-                        <td>
-                          <div className="flex align-items-center list-user-action">
-                            <Link
-                              className="btn btn-sm btn-icon btn-warning"
-                              data-toggle="tooltip"
-                              data-placement="top"
-                              title="Edit"
-                              data-original-title="Edit"
-                              to={
-                                "/FicheEleve/" +
-                                item.niveau_classe +
-                                "/" +
-                                item.intitule_classe
-                              }
-                            >
-                              <span className="btn-inner">
-                                <svg
-                                  width="20"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  ></path>
-                                  <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  ></path>
-                                  <path
-                                    d="M15.1655 4.60254L19.7315 9.16854"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  ></path>
-                                </svg>
-                              </span>
-                            </Link>{" "}
-                            
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
+                                                    {eleves_classe.map((item) => (
+                                                        <tr key={item.id}>
+                                                            <td>{item.matricule}</td>
+                                                            <td>{item.nom}</td>
+                                                            <td>{item.prenom}</td>
+                                                            <td>{item.sexe}</td>
+                                                            <td>{item.date_naissance}</td>
+                                                            <td>{item.lieu_naissance}</td>
+                                                            <td>{item.nom_parent}</td>
+                                                            <td>{item.numero_parent}</td>
+                                                            <td>{item.email}</td>
+                                                            <td>{item.telephone}</td>
+                                                            <td>
+                                                                <div className="flex align-items-center list-user-action">
+                                                                    <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Edit/" + niveau + "/" + item.id}>
+                                                                        <span className="btn-inner">
+                                                                            <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                            </svg>
+                                                                        </span>
+                                                                    </Link>{' '}
+                                                                    <Link className="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="Delete" data-original-title="Delete" onClick={() => { deleteEleves_classe(item.id) }}>
+                                                                        <span className="btn-inner">
+                                                                            <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+                                                                                <path d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                <path d="M20.708 6.23975H3.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                <path d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                            </svg>
+                                                                        </span>
+                                                                    </Link>{' '}
+                                                                </div>
+                                                            </td>
+
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
                   <tfoot></tfoot>
                 </Table>
               </div>
