6a3b71335aa1b49c4573f11f2ae122840fe3111e
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-09 11:20:34 +0100

    Update listelevesforclasse.js

--- src/views/fondateur/documents/listelevesforclasse.js
+++ src/views/fondateur/documents/listelevesforclasse.js
@@ -303,7 +303,7 @@ const ListClassesFiche = memo((props) => {
                 >
                   <thead>
                                                     <tr>
-                                                        <th>{user.langue === "en" ? (<div>Registration number</div>):(<div> Matricule </div>)}</th>
+                                                        <th>{user.langue === "en" ? (<div>Registration number</div>):(<div> {classe} </div>)}</th>
                                                         <th>{user.langue === "en" ? (<div>Name</div>):(<div> Nom  </div>)}</th>
                                                         <th>{user.langue === "en" ? (<div>Prename</div>):(<div> Prénom  </div>)}</th>
                                                         <th>{user.langue === "en" ? (<div>Gender </div>):(<div> Genre  </div>)}</th>
