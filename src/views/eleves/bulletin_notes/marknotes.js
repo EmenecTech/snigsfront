cb7cd8c78455851abafa76d4080f67396f334972
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-11 14:29:55 +0200

    Update marknotes.js

--- src/views/eleves/bulletin_notes/marknotes.js
+++ src/views/eleves/bulletin_notes/marknotes.js
@@ -6,11 +6,6 @@ import AuthUser from "../../../components/AuthUser.js";
 import Axios from "axios";
 import QRCode from "react-qr-code";
 
-import http from "../../../http.js";
-
-//circular
-import Circularprogressbar from "../../../components/circularprogressbar.js";
-
 // AOS
 import AOS from "aos";
 import "../../../../node_modules/aos/dist/aos";
@@ -24,33 +19,7 @@ import SwiperCore, { Navigation } from "swiper";
 
 // Import Swiper styles
 import "swiper/swiper-bundle.min.css";
-// import 'swiper/components/navigation/navigation.scss';
 
-//progressbar
-import Progress from "../../../components/progress.js";
-
-//img
-import shapes1 from "../../../assets/images/shapes/01.png";
-import shapes2 from "../../../assets/images/shapes/02.png";
-import shapes3 from "../../../assets/images/shapes/03.png";
-import shapes4 from "../../../assets/images/shapes/04.png";
-import shapes5 from "../../../assets/images/shapes/05.png";
-
-import avatars11 from '../../../assets/images/avatars/01.png'
-import avatars22 from '../../../assets/images/avatars/avtar_1.png'
-import avatars33 from '../../../assets/images/avatars/avtar_2.png'
-import avatars44 from '../../../assets/images/avatars/avtar_3.png'
-import avatars55 from '../../../assets/images/avatars/avtar_4.png'
-import avatars66 from '../../../assets/images/avatars/avtar_5.png'
-import avatars2 from '../../../assets/images/avatars/02.png'
-import avatars3 from '../../../assets/images/avatars/03.png'
-import avatars4 from '../../../assets/images/avatars/04.png'
-import avatars5 from '../../../assets/images/avatars/05.png'
-
-//Count-up
-import CountUp from "react-countup";
-
-// Redux Selector / Action
 import { useSelector } from "react-redux";
 
 // Import selectors & action from setting store
@@ -62,9 +31,6 @@ import { useReactToPrint } from "react-to-print";
 
 // install Swiper modules
 SwiperCore.use([Navigation]); 
-
-
-
 
 const BulletinByEleve = memo((props) => {
 
@@ -224,11 +190,6 @@ const BulletinByEleve = memo((props) => {
             setAllInd3(res.data);
         }); 
     };
-
-
-  
-    
-
 
     const [allind4, setAllInd4] = useState([]);
     useEffect(() => {
@@ -529,238 +490,20 @@ const BulletinByEleve = memo((props) => {
         })
     }
 
+      useSelector(SettingSelector.theme_color);
 
 
-    useSelector(SettingSelector.theme_color);
-
-    const getVariableColor = () => {
-        let prefix =
-            getComputedStyle(document.body).getPropertyValue("--prefix") || "bs-";
-        if (prefix) {
-            prefix = prefix.trim();
-        }
-        const color1 = getComputedStyle(document.body).getPropertyValue(
-            `--${prefix}primary`
-        );
-        const color2 = getComputedStyle(document.body).getPropertyValue(
-            `--${prefix}info`
-        );
-        const color3 = getComputedStyle(document.body).getPropertyValue(
-            `--${prefix}primary-tint-20`
-        );
-        const color4 = getComputedStyle(document.body).getPropertyValue(
-            `--${prefix}warning`
-        );
-        return {
-            primary: color1.trim(),
-            info: color2.trim(),
-            warning: color4.trim(),
-            primary_light: color3.trim(),
-        };
-    };
-    const variableColors = getVariableColor();
-
-    const colors = [variableColors.primary, variableColors.info];
-    useEffect(() => {
-        return () => colors;
-    });
-
-    useEffect(() => {
-        AOS.init({
-            startEvent: "DOMContentLoaded",
-            disable: function () {
-                var maxWidth = 996;
-                return window.innerWidth < maxWidth;
-            },
-            throttleDelay: 10,
-            once: true,
-         
-            offset: 10,
-        });
-    });
-    const chart1 = {
-        options: {
-            chart: {
-                fontFamily:
-                    '"Inter", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-                toolbar: {
-                    show: false,
-                },
-                sparkline: {
-                    enabled: false,
-                },
-            },
-            colors: colors,
-            dataLabels: {
-                enabled: false,
-            },
-            stroke: {
-                curve: "smooth",
-                width: 3,
-            },
-            yaxis: {
-                show: true,
-                labels: {
-                    show: true,
-                    minWidth: 19,
-                    maxWidth: 19,
-                    style: {
-                        colors: "#8A92A6",
-                    },
-                    offsetX: -5,
-                },
-            },
-            legend: {
-                show: false,
-            },
-            xaxis: {
-                labels: {
-                    minHeight: 22,
-                    maxHeight: 22,
-                    show: true,
-                    style: {
-                        colors: "#8A92A6",
-                    },
-                },
-                lines: {
-                    show: false, //or just here to disable only x axis grids
-                },
-                categories: ["Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug"],
-            },
-            grid: {
-                show: false,
-            },
-            fill: {
-                type: "gradient",
-                gradient: {
-                    shade: "dark",
-                    type: "vertical",
-                    shadeIntensity: 0,
-                    gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-                    inverseColors: true,
-                    opacityFrom: 0.4,
-                    opacityTo: 0.1,
-                    stops: [0, 50, 80],
-                    colors: colors,
-                },
-            },
-            tooltip: {
-                enabled: true,
-            },
-        },
-        series: [
-            {
-                name: "total",
-                data: [94, 80, 94, 80, 94, 80, 94],
-            },
-            {
-                name: "pipline",
-                data: [72, 60, 84, 60, 74, 60, 78],
-            },
-        ],
-    };
-
-    //chart2
-    const chart2 = {
-        options: {
-            colors: colors,
-            plotOptions: {
-                radialBar: {
-                    hollow: {
-                        margin: 10,
-                        size: "50%",
-                    },
-                    track: {
-                        margin: 10,
-                        strokeWidth: "50%",
-                    },
-                    dataLabels: {
-                        show: false,
-                    },
-                },
-            },
-            labels: ["Apples", "Oranges"],
-        },
-        series: [55, 75],
-    };
-    const chart3 = {
-        options: {
-            chart: {
-                stacked: true,
-                toolbar: {
-                    show: false,
-                },
-            },
-            colors: colors,
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: "28%",
-                    endingShape: "rounded",
-                    borderRadius: 5,
-                },
-            },
-            legend: {
-                show: false,
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            stroke: {
-                show: true,
-                width: 2,
-                colors: ["transparent"],
-            },
-            xaxis: {
-                categories: ["S", "M", "T", "W", "T", "F", "S", "M", "T", "W"],
-                labels: {
-                    minHeight: 20,
-                    maxHeight: 20,
-                    style: {
-                        colors: "#8A92A6",
-                    },
-                },
-            },
-            yaxis: {
-                title: {
-                    text: "",
-                },
-                labels: {
-                    minWidth: 19,
-                    maxWidth: 19,
-                    style: {
-                        colors: "#8A92A6",
-                    },
-                },
-            },
-            fill: {
-                opacity: 1,
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return "$ " + val + " thousands";
-                    },
-                },
-            },
-        },
-        series: [
-            {
-                name: "Successful deals",
-                data: [30, 50, 35, 60, 40, 60, 60, 30, 50, 35],
-            },
-            {
-                name: "Failed deals",
-                data: [40, 50, 55, 50, 30, 80, 30, 40, 50, 55],
-            },
-        ],
-    };
 
 
+    
+
+
+   
 
     return (
+
         
-        <Fragment>
+    <Fragment>
         
         {classes.cycle_niveau === 'Secondaire' || classes.cycle_niveau === 'Secondary' ? <div>
   
