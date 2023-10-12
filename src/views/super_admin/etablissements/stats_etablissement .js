f85400582622b2595dcc7562c6de2903393bdbd7
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-12 12:55:55 +0100

    Update stats_etablissement .js

--- src/views/super_admin/etablissements/stats_etablissement .js
+++ src/views/super_admin/etablissements/stats_etablissement .js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, memo, Fragment } from "react";
 import { Row, Col, Dropdown, Button, Form, Table } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, createPath, useNavigate  } from "react-router-dom";
 import AuthUser from "../../../components/AuthUser.js";
 
 //circular
