dc7477ae0e853af4e2f049cc1b65420c2bdd2542
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-13 16:53:24 +0100

    Update classe primaire.js

--- src/views/fondateur/classes/classe primaire.js
+++ src/views/fondateur/classes/classe primaire.js
@@ -562,7 +562,7 @@ const PrimClasse = () => {
                                                                     <Form.Label htmlFor="exampleInputText1"> {user.langue === "en" ? (<div> Group </div>):(<div>Groupe</div>)} </Form.Label>
                                                                     
     {etab === 25 ? <div>
-    <select className="form-select mb-3 shadow-none" name="int" onChange={handleChange} required>
+    <select className="form-select mb-3 shadow-none" name="groupe" onChange={handleChange} required>
                                                                         <option></option>
 
     <option value="Communication in English"> {user.langue === "en" ? (<div> Group 1</div>):(<div>Communication in English</div>)} </option>
@@ -744,7 +744,7 @@ const PrimClasse = () => {
 
                                                                     </select>
     </div> : <div>
-    <select className="form-select mb-3 shadow-none" name="groupe" onChange={handleChange} required>
+    <select className="form-select mb-3 shadow-none" name="int" onChange={handleChange} required>
                                                                         <option></option>
 
     <option value="Groupe 1"> {user.langue === "en" ? (<div> Group 1</div>):(<div>Groupe 1</div>)} </option>
