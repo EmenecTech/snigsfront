f0a6d61e435b748280a467d69ee7fa220d8a1cde
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-16 17:18:15 +0200

    Update bulletin_eleve.js

--- src/views/fondateur/bulletins/bulletin_eleve.js
+++ src/views/fondateur/bulletins/bulletin_eleve.js
@@ -2863,7 +2863,7 @@ const BulletinByEleve = memo((props) => {
                                                     </Row>
                                                     <Row>
                                                         <div className="table-responsive border-bottom my-3">
-                                                         {allnoteprim_1.existgrp === 0 || allnoteprim_1.existgrp ? (<div>
+                                                         {allnoteprim_1.groupe === "No" || allnoteprim_1.groupe ? (<div>
                                                             <Table
                                                                 responsive
                                                                 striped
