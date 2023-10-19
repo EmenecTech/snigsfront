e88a44779a9240ba9e48567adf771f07c30992f2
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-19 12:59:21 +0100

    Update matieres.js

--- src/views/fondateur/notes/matieres.js
+++ src/views/fondateur/notes/matieres.js
@@ -3,7 +3,7 @@ import { Row, Col, Dropdown, Modal, Button, Table, Form } from "react-bootstrap"
 import { createPath, useNavigate, useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import AuthUser from "../../../components/AuthUser.js";
-
+ 
 
 import http from "../../../http.js";
 
@@ -48,35 +48,33 @@ import Card from "../../../components/Card.js";
 // install Swiper modules
 SwiperCore.use([Navigation]);
 
-
-
-
-const AdminMatieresNotesEleves = memo((props) => {
+const EnseignantListEleves = memo((props) => {
     const [show, setShow] = useState(false);
     const { user, http } = AuthUser();
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({});
-    const { niveau, classe, evaluation } = useParams();
     const etab = user.etablissement;
+    const userid = user.id;
+    const {niveau, classe, evaluation} = useParams();
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
 
-
-
-    const [matieres_classe, setmatieres_classe] = useState([]);
-    useEffect(() => {
-        fetchAllmatieres_classe();
-    }, []);
-
-    const fetchAllmatieres_classe = () => {
-        http.get('/get_matieres_for_classe/' + etab + '/' + niveau + '/' + classe).then(res => {
-            setmatieres_classe(res.data);
-        })
+        setInputs(values => ({ ...values, [name]: value, etab }))
     }
 
+  const [eleves_classe, seteleves_classe] = useState([]);
+    useEffect(() => {
+        fetchAlleleves_classe();
+    }, []);
 
-
-
+    const fetchAlleleves_classe = () => {
+        http.get('/fetch_eleves_in_classe/' + etab + '/' + classe).then(res => {
+            seteleves_classe(res.data);
+        })
+    }
 
     useSelector(SettingSelector.theme_color);
 
@@ -303,102 +301,70 @@ const AdminMatieresNotesEleves = memo((props) => {
         ],
     };
 
-
-
-
-
-
-
-
-
-
-    return (
+return (
         <Fragment>
             <Row>
                 <Col sm="12">
                     <Card>
                         <Card.Header className="d-flex justify-content-between">
                             <div className="header-title">
-                                <h4 className="card-title"></h4>
+                                <h4 className="card-title">Eleves</h4>
                             </div>
+
 
 
                         </Card.Header>
-                        <Card.Body>
+                         <Card.Body>
+                            <div className="table-responsive border-bottom my-3">
+                                <Table
+                                    responsive
+                                    striped
+                                    id="datatable"
+                                    className=""
+                                    data-toggle="data-table"
+                                >
+                                    <thead>
+                                        <tr>
 
-                            <div>
-                                <Row>
-                                    <Col sm="12" lg="12">
-                                        <Card>
-                                            <Card.Header className="d-flex justify-content-between">
-                                                <div className="header-title">
-                                                    <h4 className="card-title">{user.langue === "en" ? (<div>Courses</div>):(<div> Matières </div>)}</h4>
-                                                </div>
-                                            </Card.Header>
-                                            <Card.Body>
-                                                <div className="table-responsive border-bottom my-3">
-                                                    <Table
-                                                        responsive
-                                                        striped
-                                                        id="datatable"
-                                                        className=""
-                                                        data-toggle="data-table"
-                                                    >
-                                                        <thead>
-                                                            <tr>
-                                                                <th>{user.langue === "en" ? (<div>Course</div>):(<div> Matiere  </div>)}</th>
-                                                                <th>Coefficient </th>
-                                                                <th>{user.langue === "en" ? (<div>Group</div>):(<div> Groupe   </div>)}</th>
-                                                                <th>Action</th>
+                                            <th>Eleves </th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {eleves_classe.map((user) => (
+                                            <tr key={user.id}>
+                                                <td>{user.nom} {user.prenom}</td>
+                                                <td>
+                                                    <div className="flex align-items-center list-user-action">
 
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            {matieres_classe.map((item) => (
-                                                                <tr key={item.id}>
-                                                                    <td>{item.matiere_cm}</td>
-                                                                    <td>{item.coefficient_cm}</td>
-                                                                    <td>{item.groupe_cm}</td>
-                                                                    <td>
-                                                                        <div className="flex align-items-center list-user-action">
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Enseignant/List/Eleves/Groupes/" + niveau + "/" + classe + "/" + evaluation + "/" + user.id }>
+                                                            <span className="btn-inner">
+                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                </svg>
+                                                            </span>
+                                                        </Link>
 
-                                                                            <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Admin/Edit/Notes/" + niveau + "/" + classe + "/" + evaluation + "/" + item.matiere_cm}>
-                                                                                <span className="btn-inner">
-                                                                                    <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                                        <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                                        <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                                        <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                                    </svg>
-                                                                                </span>
-                                                                            </Link>{' '}
-                                                                        </div>
-                                                                    </td>
+                                                    </div>
+                                                </td>
 
-                                                                </tr>
-                                                            ))}
-                                                        </tbody>
-                                                        <tfoot>
+                                            </tr>
+                                   ))}
+                                    </tbody>
+                                    <tfoot>
 
-                                                        </tfoot>
-                                                    </Table>
-
-                                                </div>
-                                            </Card.Body>
-                                        </Card>
-                                    </Col>
-
-
-
-                                </Row>
-
+                                    </tfoot>
+                                </Table>
 
                             </div>
                         </Card.Body>
-                    </Card>
+                </Card>
                 </Col>
             </Row>
         </Fragment>
     );
 })
 
-export default AdminMatieresNotesEleves
+export default EnseignantListEleves
