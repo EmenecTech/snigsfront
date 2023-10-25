fd1b24fbd097101c6e2bf827cdc37e0037fcfecc
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-25 10:50:03 +0100

    Update bulletin_eleve.js

--- src/views/fondateur/bulletins/bulletin_eleve.js
+++ src/views/fondateur/bulletins/bulletin_eleve.js
@@ -2861,6 +2861,20 @@ const BulletinByEleve = memo((props) => {
                                                                 </Row>
                                                                 <Row>
 
+                                                                    <Col sm="12" lg="12">
+
+                                                                        <div className="mt-2">
+                                                                            <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant: {enseign} </p>
+                                                                        </div>
+
+                                                                        <div className="mt-2">
+                                                                            <p tyle={{ fontSize: "10px" }} className="mb-0"><strong>Nom : {elevesinclass} </strong> </p>
+                                                                        </div>
+
+                                                                    </Col>
+                                                                </Row>
+                                                                <Row>
+
                                                                     <Table
                                                                         responsive
                                                                         striped
@@ -5176,22 +5190,86 @@ const BulletinByEleve = memo((props) => {
                                                                 </Card.Header>
                                                                 <Card.Body>
                                                                     <div>
+                                                         <Row>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">REPUBLIQUE DU CAMEROUN <br />
+                                                                            Paix - Travail - Patrie <br />
+                                                                            MINISTERE DE L'EDUCATION DE BASE<br />
+
+                                                                        </p>
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" >
+                                                                        <Row>
+                                                                            <Col sm="12" lg="12" className="d-flex justify-content-center">
+                                                                                <div className="user-profile">
+                                                                                    <Image className="theme-color-default-img  rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-purple-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-blue-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-green-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-yellow-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-pink-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                </div>
+                                                                            </Col>
+                                                                        </Row>
+                                                                        <br />
+                                                                        <Row>
+                                                                            <Col sm="12">
+                                                                                <div className="container text-center">
+                                                                                    <p> <strong style={{ fontSize: "18px" }}> {info_etab.nom_etablissement} </strong>
+
+                                                                                        <br />
+                                                                                        <smal>BP:{info_etab.bp_etablissement} Tel:{info_etab.telephone}</smal>
+                                                                                    </p>
+
+                                                                                </div>
+                                                                            </Col>
+                                                                        </Row>
+
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">REPUBLIC OF CAMEROON <br />
+                                                                            Peace - Work - Fatherland <br />
+                                                                            MINISTRY OF BASIC EDUCATION<br />
+
+                                                                        </p>
+
+
+                                                                    </Col>
+
+                                                                </Row>
+                                                                <Row className="mt-2">
+                                                                    <Col sm="4" lg="4">
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">
+                                                                            <span style={{ fontSize: "15px" }}> Report Card {evaluation}</span>
+                                                                            <br />
+                                                                            2022 - 2023
+                                                                        </p>
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+
+
+                                                                    </Col>
+
+                                                                </Row>
 
                                                                         <Row>
 
                                                                             <Col sm="12" lg="12">
-                                                                                <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Evaluation: {evaluation} </p>
-
-                                                                                </div>
+                                                                                
                                                                                 <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0">Teacher's name: {enseign} </p>
 
-                                                                                </div>
-                                                                                <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Repeater:</p>
-
-                                                                                </div>
+                                                                                
                                                                                 <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0"><strong>Name : {elevesinclass} </strong> </p>
 
@@ -5966,21 +6044,83 @@ const BulletinByEleve = memo((props) => {
                                                                 <Card.Body>
                                                                     <div>
 
+                                                         <Row>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">REPUBLIQUE DU CAMEROUN <br />
+                                                                            Paix - Travail - Patrie <br />
+                                                                            MINISTERE DE L'EDUCATION DE BASE<br />
+
+                                                                        </p>
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" >
+                                                                        <Row>
+                                                                            <Col sm="12" lg="12" className="d-flex justify-content-center">
+                                                                                <div className="user-profile">
+                                                                                    <Image className="theme-color-default-img  rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-purple-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-blue-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-green-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-yellow-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                    <Image className="theme-color-pink-img rounded avatar-130 img-fluid" src={"http://127.0.0.1:8000/logo_etab/" + etab + ".png"} alt="profile-pic" />
+                                                                                </div>
+                                                                            </Col>
+                                                                        </Row>
+                                                                        <br />
+                                                                        <Row>
+                                                                            <Col sm="12">
+                                                                                <div className="container text-center">
+                                                                                    <p> <strong style={{ fontSize: "18px" }}> {info_etab.nom_etablissement} </strong>
+
+                                                                                        <br />
+                                                                                        <smal>BP:{info_etab.bp_etablissement} Tel:{info_etab.telephone}</smal>
+                                                                                    </p>
+
+                                                                                </div>
+                                                                            </Col>
+                                                                        </Row>
+
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">REPUBLIC OF CAMEROON <br />
+                                                                            Peace - Work - Fatherland <br />
+                                                                            MINISTRY OF BASIC EDUCATION<br />
+
+                                                                        </p>
+
+
+                                                                    </Col>
+
+                                                                </Row>
+                                                                <Row className="mt-2">
+                                                                    <Col sm="4" lg="4">
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+                                                                        <p className="text-center">
+                                                                            <span style={{ fontSize: "15px" }}> Report Card {evaluation}</span>
+                                                                            <br />
+                                                                            2022 - 2023
+                                                                        </p>
+
+
+                                                                    </Col>
+                                                                    <Col sm="4" lg="4">
+
+
+                                                                    </Col>
+
+                                                                </Row>
                                                                         <Row>
 
                                                                             <Col sm="12" lg="12">
-                                                                                <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Evaluation: {evaluation} </p>
-
-                                                                                </div>
+                                                            
                                                                                 <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0">Teacher's name: {enseign} </p>
-
-                                                                                </div>
-                                                                                <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Repeater:</p>
-
-                                                                                </div>
                                                                                 <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0"><strong>Name : {elevesinclass} </strong> </p>
 
@@ -7486,24 +7626,11 @@ const BulletinByEleve = memo((props) => {
 
                                                                             <Col sm="12" lg="12">
                                                                                 <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Evaluation: {evaluation} </p>
-
-                                                                                </div>
-                                                                                <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0">Nom de l'enseignant:  {enseign} </p>
-
-                                                                                </div>
-                                                                                <div className="mt-2">
-                                                                                    <p tyle={{ fontSize: "10px" }} className="mb-0">Redoublant: </p>
-                                                                                    <p>Repeater</p>
                                                                                 </div>
                                                                                 <div className="mt-2">
                                                                                     <p tyle={{ fontSize: "10px" }} className="mb-0"><strong>Nom : {elevesinclass} </strong> </p>
-
                                                                                 </div>
-
-
-
                                                                             </Col>
 
 
