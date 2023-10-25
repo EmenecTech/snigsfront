5d67e9fbdfd914b5c8c71dfab089d401d88edd90
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-25 12:29:07 +0200

    Update listgroupes.js

--- src/views/enseignant/notes/listgroupes.js
+++ src/views/enseignant/notes/listgroupes.js
@@ -115,7 +115,7 @@ const EnseignantListGroupes = () => {
 
         })
 
-        window.location.reload(false);
+        //window.location.reload(false);
     }
 
     const [matieres, setmatieres] = useState([]);
