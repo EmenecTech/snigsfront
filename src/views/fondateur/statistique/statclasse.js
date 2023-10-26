345933c7916e50fcc01a4b99b65b139eb40356b0
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-10-26 12:12:54 +0100

    Update statclasse.js

--- src/views/fondateur/statistique/statclasse.js
+++ src/views/fondateur/statistique/statclasse.js
@@ -95,7 +95,7 @@ const ClassesStat = memo((props) => {
       }, []);
     
       const fetchAlltotal_filles = () => {
-        http.get("/nbreleve_fille_classe/" + etab + '/' + classe).then((res) => {
+        http.get("/nbreleve_fille_classe/" + etabs + '/' + classe).then((res) => {
           settotal_filles(res.data);
         });
       };
