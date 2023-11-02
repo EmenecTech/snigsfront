394ea2f60866ca25fd9965bbbb35b0257cc97e2b
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-02 16:06:48 +0100

    Update statclasse.js

--- src/views/fondateur/statistique/statclasse.js
+++ src/views/fondateur/statistique/statclasse.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, memo, Fragment, useRef } from "react";
-import { Row, Col, Dropdown, Modal, Button, Table, Form } from "react-bootstrap";
+import { Row, Col, Dropdown, Modal, Button, Table, Form, Image } from "react-bootstrap";
 import { createPath, useNavigate, useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import AuthUser from "../../../components/AuthUser.js";
