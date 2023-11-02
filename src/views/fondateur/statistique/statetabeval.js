88eda88b4957bc149a7f9363ed366af087da4603
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-02 09:42:14 +0100

    Update statetabeval.js

--- src/views/fondateur/statistique/statetabeval.js
+++ src/views/fondateur/statistique/statetabeval.js
@@ -389,7 +389,7 @@ const ListEtabEvalStat = memo((props) => {
 
                                             <tr >
                                                 <td>Plus grande Moyenne de la classe</td>
-                                                <td>{firsteleves.moyen}</td>
+                                                <td>{firsteleves}</td>
 
                                             </tr>
                                           
