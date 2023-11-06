76c11471a9d3d3e049acd478d3a56ef55f0a5939
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-06 02:27:05 +0100

    Update statclasse.js

--- src/views/fondateur/statistique/statclasse.js
+++ src/views/fondateur/statistique/statclasse.js
@@ -379,16 +379,7 @@ const ClassesStat = memo((props) => {
                     <Card>
 
                         <Card.Header className="d-flex justify-content-between">
-
-
-                            <Button variant="primary mt-2" onClick={printData}>
-                                <span className="btn-inner">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                    </svg>
-                                </span>
-                                Imprimer
-                            </Button>
+                        <Button variant="primary mt-2" onClick={printData}> Imprimer </Button>
 
                         </Card.Header>
                         <Card.Body>
