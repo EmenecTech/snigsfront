e22fe8cf95ac18c151cc5f5715715e7584b8193d
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-06 02:52:48 +0100

    Update evaluationsetab.js

--- src/views/fondateur/statistique/evaluationsetab.js
+++ src/views/fondateur/statistique/evaluationsetab.js
@@ -335,8 +335,7 @@ const StatListEvaluationsEtab = memo((props) => {
                                             <th>Intitulé</th>
                                             <th>Type d'évaluation</th>
                                             <th>Type de période</th>
-                                            <th>Date de début</th>
-                                            <th>Date de fin</th>
+                                           
                                             <th>Action</th>
                                         </tr>
                                     </thead>
@@ -347,8 +346,7 @@ const StatListEvaluationsEtab = memo((props) => {
 
                                                 <td>{item.type_evaluation_evaluation}</td>
                                                 <td>{item.reference_evaluation}</td>
-                                                <td>/</td>
-                                                <td>/</td>
+                                               
                                                 <td>
                                                     <Link className="btn btn-sm btn-icon btn-info" data-toggle="tooltip" data-placement="top" title="Delete" data-original-title="Delete" to={"/Admin/Stat/etablissement/" + item.intitule_evaluation}>
 
