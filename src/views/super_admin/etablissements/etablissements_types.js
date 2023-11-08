0433f7e9330c95c06bd9efd08c6367564d8a639c
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-08 16:36:16 +0100

    Update etablissements_types.js

--- src/views/super_admin/etablissements/etablissements_types.js
+++ src/views/super_admin/etablissements/etablissements_types.js
@@ -325,12 +325,7 @@ const ListEtablissementsType = memo((props) => {
                             </div>
 
                             <Link to="#">
-                                <Button type="button" variant="primary">
-                                    <span className="btn-inner">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                        </svg>
-                                    </span>
+                                <Button type="button" variant="primary">                       
                                     Imprimer
                                 </Button>
                             </Link>
