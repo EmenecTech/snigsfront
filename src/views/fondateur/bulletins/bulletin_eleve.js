fc036a575f7d031f5bb2d4f3c1742692d69b2882
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-15 15:28:09 +0100

    Update bulletin_eleve.js

--- src/views/fondateur/bulletins/bulletin_eleve.js
+++ src/views/fondateur/bulletins/bulletin_eleve.js
@@ -225,6 +225,8 @@ const BulletinByEleve = memo((props) => {
             setmatiereslevel(res.data);
         })
     }
+
+
     const [classes, setclasses] = useState([]);
     useEffect(() => {
         fetchAllclasses();
@@ -3571,7 +3573,7 @@ const BulletinByEleve = memo((props) => {
                                                                         {allnoteprim_1.existgrp > 0 && (
                                                                             <thead>
                                                                                 <tr>
-                                                                                    <th>{allnoteprim_1.groupe} </th>
+                                                                                    <th>{allnoteprim_1.groupe} (/ {allnoteprim_1.note_groupe}) </th>
                                                                                     <th>{allnoteprim_1.sumnote}</th>
                                                                                     <th></th>
                                                                                 </tr>
@@ -3591,7 +3593,7 @@ const BulletinByEleve = memo((props) => {
                                                                         {allnoteprim_2.existgrp > 0 && (
                                                                             <thead>
                                                                                 <tr>
-                                                                                    <th>{allnoteprim_2.groupe} </th>
+                                                                                    <th>{allnoteprim_2.groupe} (/ {allnoteprim_2.note_groupe}) </th>
                                                                                     <th>{allnoteprim_2.sumnote}</th>
                                                                                     <th></th>
                                                                                 </tr>
@@ -3612,7 +3614,7 @@ const BulletinByEleve = memo((props) => {
                                                                         {allnoteprim_3.existgrp > 0 && (
                                                                             <thead>
                                                                                 <tr>
-                                                                                    <th>{allnoteprim_3.groupe}</th>
+                                                                                    <th>{allnoteprim_3.groupe} (/ {allnoteprim_3.note_groupe}) </th>
                                                                                     <th>{allnoteprim_3.sumnote}</th>
                                                                                     <th></th>
                                                                                 </tr>
@@ -3633,7 +3635,7 @@ const BulletinByEleve = memo((props) => {
                                                                             <thead>
                                                                                 <tr>
 
-                                                                                    <th>{allnoteprim_4.groupe}</th>
+                                                                                    <th>{allnoteprim_4.groupe} (/ {allnoteprim_4.note_groupe}) </th>
                                                                                     <th>{allnoteprim_4.sumnote}</th>
                                                                                     <th></th>
 
@@ -3655,7 +3657,7 @@ const BulletinByEleve = memo((props) => {
                                                                             <thead>
                                                                                 <tr>
 
-                                                                                    <th>{allnoteprim_5.groupe}</th>
+                                                                                    <th>{allnoteprim_5.groupe} (/ {allnoteprim_5.note_groupe}) </th>
                                                                                     <th>{allnoteprim_5.sumnote}</th>
                                                                                     <th></th>
 
