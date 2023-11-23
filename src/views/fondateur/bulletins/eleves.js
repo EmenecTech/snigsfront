8113c8aec1bda3256b07d970bd0db8f5ce01d7eb
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-23 14:53:01 +0100

    Update eleves.js

--- src/views/fondateur/bulletins/eleves.js
+++ src/views/fondateur/bulletins/eleves.js
@@ -374,7 +374,9 @@ const AdminEvaluationListClassesEleves = memo((props) => {
                                                             <td>
                                                     <div className="flex align-items-center list-user-action">
 
-                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Admin/BulletinDeNotes/" + evaluation + "/"  + niveau  + "/" + item.other_in_user + "/" + item.id }>
+                                                        {eleves_classe.cycle.niveau === 'Secondaire' || eleves_classe.cycle.niveau === 'Secondary' ? <div>
+
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Admin/Bulletin/Secondaire" + evaluation + "/"  + niveau  + "/" + item.other_in_user + "/" + item.id }>
                                                             <span className="btn-inner">
                                                                 <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                     <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -383,6 +385,36 @@ const AdminEvaluationListClassesEleves = memo((props) => {
                                                                 </svg>
                                                             </span>
                                                         </Link>{' '}
+
+                                                        </div>:<div></div>}
+
+                                                        {eleves_classe.cycle.niveau === 'Primaire' || eleves_classe.cycle.niveau === 'Primary' ? <div>
+
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Admin/Bulletin/Primaire" + evaluation + "/"  + niveau  + "/" + item.other_in_user + "/" + item.id }>
+                                                            <span className="btn-inner">
+                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                </svg>
+                                                            </span>
+                                                        </Link>{' '}
+
+                                                        </div>:<div></div>}
+
+                                                        {eleves_classe.cycle.niveau === 'Maternelle' || eleves_classe.cycle.niveau === 'Nursery' ? <div>
+
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Admin/Bulletin/Maternelle" + evaluation + "/"  + niveau  + "/" + item.other_in_user + "/" + item.id }>
+                                                            <span className="btn-inner">
+                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                </svg>
+                                                            </span>
+                                                        </Link>{' '}
+
+                                                        </div>:<div></div>}
                                                         
                                                     </div>
                                                 </td>
