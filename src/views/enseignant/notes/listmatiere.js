8bf7dbcad513b0e947cdd4aeab4d01eb5fcde09b
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-29 12:08:00 +0100

    Update listmatiere.js

--- src/views/enseignant/notes/listmatiere.js
+++ src/views/enseignant/notes/listmatiere.js
@@ -365,7 +365,7 @@ const Enseignant_matieres_notes = memo((props) => {
                         </Card.Header>
                       
         
-     {confnotes.niveau_enseignement === "Secondaire" ? 
+     {confnotes.niveau_enseignement === "Secondaire" || confnotes.niveau_enseignement === "Secondary"? 
          
             <div>
                 <Card.Body>
@@ -417,57 +417,120 @@ const Enseignant_matieres_notes = memo((props) => {
                             </div>
                         </Card.Body>
                                        
-</div>  :   <div> 
+</div> : <div></div> } 
+
+                      
+        
+{confnotes.niveau_enseignement === "Licence" || confnotes.niveau_enseignement === "Master" ? 
+         
+         <div>
+             <Card.Body>
+                         <div className="table-responsive border-bottom my-3">
+                             <Table
+                                 responsive
+                                 striped
+                                 id="datatable"
+                                 className=""
+                                 data-toggle="data-table"
+                             >
+                                 <thead>
+                                     <tr>
+
+                                         <th>Classe</th>
+                                         <th>Unité d'Enseigenement(UE)</th>
+                                         <th>Action</th>
+                                     </tr>
+                                 </thead>
+                                 <tbody>
+                                     {matieres.map((item) => (
+                                         <tr key={item.id}>
+                                             <td>{item.classe_cp}</td>
+                                             <td>{item.matiere_cp}</td>
+                                             <td>
+                                                 <div className="flex align-items-center list-user-action">
+
+                                                     <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Professeur/List/Evaluations/" + item.classe_cp + "/" + item.matiere_cp }>
+                                                         <span className="btn-inner">
+                                                             <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                 <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                 <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                 <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                             </svg>
+                                                         </span>
+                                                     </Link>
+
+                                                 </div>
+                                             </td>
+
+                                         </tr>
+                                     ))}
+                                 </tbody>
+                                 <tfoot>
+
+                                 </tfoot>
+                             </Table>
+
+                         </div>
+                     </Card.Body>
+                                    
+</div> : <div></div> } 
+
+
+
+{confnotes.niveau_enseignement === "Primaire" || confnotes.niveau_enseignement === "Primary" || confnotes.niveau_enseignement === "Maternelle" || confnotes.niveau_enseignement === "Nursery" ? <div>
+
+                <Card.Body>
+                                            <div className="table-responsive border-bottom my-3">
+                                                <Table
+                                                    responsive
+                                                    striped
+                                                    id="datatable"
+                                                    className=""
+                                                    data-toggle="data-table"
+                                                >
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Classe</th>
+                                                            <th>Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {matieresprim.map((item) => (
+                                                            <tr key={item.id}>
+                                                                <td>{item.classe_cp}</td>
+                                                            
+                                                                <td>
+                                                                    <div className="flex align-items-center list-user-action">
+
+                                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Enseignant/List/Groupes/Evaluations/" + item.classe_cp }>
+                                                                            <span className="btn-inner">
+                                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                                                </svg>
+                                                                            </span>
+                                                                        </Link>
+
+                                                                    </div>
+                                                                </td>
+
+                                                            </tr>
+                                                        ))}
+                                                    </tbody>
+                                                    <tfoot>
+
+                                                    </tfoot>
+                                                </Table>
+
+                                            </div>
+                                        </Card.Body>
+
+</div>:<div></div>}
                                                   
-                        <Card.Body>
-                            <div className="table-responsive border-bottom my-3">
-                                <Table
-                                    responsive
-                                    striped
-                                    id="datatable"
-                                    className=""
-                                    data-toggle="data-table"
-                                >
-                                    <thead>
-                                        <tr>
-
-                                            <th>Classe</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {matieresprim.map((item) => (
-                                            <tr key={item.id}>
-                                                <td>{item.classe_cp}</td>
-                                               
-                                                <td>
-                                                    <div className="flex align-items-center list-user-action">
-
-                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Enseignant/List/Groupes/Evaluations/" + item.classe_cp }>
-                                                            <span className="btn-inner">
-                                                                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                    <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                    <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                                </svg>
-                                                            </span>
-                                                        </Link>
-
-                                                    </div>
-                                                </td>
-
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                    <tfoot>
-
-                                    </tfoot>
-                                </Table>
-
-                            </div>
-                        </Card.Body>
+                     
                                                  
-        </div> } 
+      
        
     
                     </Card>
