121462a700b0c9e9b87a6fb2498abd5630d4f69b
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-11-29 12:29:43 +0100

    Update notesevaluations.js

--- src/views/universite/professeurs/notes/notesevaluations.js
+++ src/views/universite/professeurs/notes/notesevaluations.js
@@ -51,7 +51,7 @@ SwiperCore.use([Navigation]);
 
 
 
-const EnseignantListEvaluations = memo((props) => {
+const ProfesseurListEvaluations = memo((props) => {
     const [show, setShow] = useState(false);
     const { user, http } = AuthUser();
     const handleClose = () => setShow(false);
@@ -392,7 +392,7 @@ const EnseignantListEvaluations = memo((props) => {
                                                 <td>
                                                     <div className="flex align-items-center list-user-action">
 
-                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Enseignant/List/Notes/" + item.intitule_evaluation + "/" + niveau + "/" + classe + "/" + matiere}>
+                                                        <Link className="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Professeur/List/Notes/" + item.intitule_evaluation + "/" + niveau + "/" + classe + "/" + matiere}>
                                                             <span className="btn-inner">
                                                                 <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                     <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -423,4 +423,4 @@ const EnseignantListEvaluations = memo((props) => {
     );
 })
 
-export default EnseignantListEvaluations
+export default ProfesseurListEvaluations
