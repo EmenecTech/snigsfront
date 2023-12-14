bfed01a3dc0b73cd76a9aa2e8a581bebbddb59b1
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-12-14 15:36:47 +0100

    Update noteseleve.js

--- src/views/parents/pedagogie/notes/noteseleve.js
+++ src/views/parents/pedagogie/notes/noteseleve.js
@@ -95,7 +95,7 @@ const ParentsListNotesEval = () => {
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value, etab, classe, matiere, evaluation}))
+        setInputs(values => ({ ...values, [name]: value, etab, classe, evaluation}))
 
     }
 
