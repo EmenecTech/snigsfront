93687faa97a6b119ff06ac07379f7d0f478151e2
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-12-14 11:49:28 +0100

    Update evaluations.js

--- src/views/parents/bulletin_notes/evaluations.js
+++ src/views/parents/bulletin_notes/evaluations.js
@@ -346,6 +346,11 @@ const [classes, setclasses] = useState([]);
                                                          Consulter 
                                                          </Link>
                                                     </div>:<div></div>}
+                          {classes.cycle_niveau === 'Licence' ? <div>
+                                                         <Link className="btn btn-sm btn-icon btn-info" data-toggle="tooltip" data-placement="top" title="Edit" data-original-title="Edit" to={"/Parents/Bulletin/Universitaire/" + item.intitule_evaluation}>
+                                                         Consulter 
+                                                         </Link>
+                                                    </div>:<div></div>}
 
                         </td>
                       </tr>
