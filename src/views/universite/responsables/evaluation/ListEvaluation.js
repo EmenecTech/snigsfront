d90b8482c4f1038d4492486059b7e88058cf365a
EmenecTech <138142334+EmenecTech@users.noreply.github.com>
2023-12-20 13:16:19 +0100

    Update ListEvaluation.js

--- src/views/universite/responsables/evaluation/ListEvaluation.js
+++ src/views/universite/responsables/evaluation/ListEvaluation.js
@@ -333,7 +333,7 @@ const ListEvaluation = memo((props) => {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                     </svg>
                                 </span>
-                                Ajouter
+                                {user.langue === "en" ? (<div> Add </div>):(<div>Ajouter</div>)}
                             </Button>
 
                              
